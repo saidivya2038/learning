5e2385567fc5ad7c2ee9b009d9ad530f6deedd27
saidivya2038 <saidivya2038@gmail.com>
2020-08-17 18:09:22 -0500

    updated files

--- challenger2/src/main/java/javaApp/PrimeSum.java
+++ challenger2/src/main/java/javaApp/PrimeSum.java
@@ -7,10 +7,29 @@ public class PrimeSum {
     }
 
     static long primesum(int maxNum) {
-        int sum = 0, num;
+      if(maxNum<=0)
+      {
+        return 0;
+      }
+        int sum = 0,count, i,num;
+        for (num=1;num<=maxNum;num++)
+        {
+          count=0;
+          for (i=2;i<=num/2;i++)
+          if (num%i==0)
+          {
+            count ++;
+            break;
+
+          }
+          if (count==0 && num!=1)
+          {
+          sum=sum+num;
+        }
+      }
         //insert your code here
 
 
         return sum;
     }
-} 
+}
